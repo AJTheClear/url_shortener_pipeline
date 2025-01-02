b0fc670bd309d7951ba33c9ee9928d3a59871f88
AJTheClear <jasen.bs@gmail.com>
2025-01-02 18:57:25 +0200

    added url class

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class URL(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    original_url = db.Column(db.String, nullable=False)
+    short_url = db.Column(db.String, unique=True, nullable=False)
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
