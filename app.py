db593ee9d6b50a9edbaf60fbb5b2abb0ea80c357
AJTheClear <jasen.bs@gmail.com>
2025-01-02 18:10:45 +0200

    added index page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from Flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'This is the beginning of a flask app'
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
