35476bcf256c2e01a1dd85fa14e095c24c952a12
AJTheClear <jasen.bs@gmail.com>
2025-01-04 21:41:27 +0200

    let's see if it will run this time

--- setup_db.py
+++ setup_db.py
@@ -1,11 +1,6 @@
 from app import app, db
-from flask import Flask
-import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__)
-url = 'sqlite:///' + os.path.join(basedir, 'instance', 'urls.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = url
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/urls.db'  # Ensure this is set
 
-with app.app_context():
-    db.create_all()
+with app.app_context():  # Create an application context
+    db.create_all()  # Create all tables
