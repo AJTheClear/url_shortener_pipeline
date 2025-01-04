ffb8b4f4bbb4ff7b68ac07cc932b22cea85d79a6
AJTheClear <jasen.bs@gmail.com>
2025-01-04 21:37:12 +0200

    changes to db init

--- setup_db.py
+++ setup_db.py
@@ -1,6 +1,11 @@
 from app import app, db
+from flask import Flask
+import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+url = 'sqlite:///' + os.path.join(basedir, 'instance', 'urls.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = url
 
 with app.app_context():
     db.create_all()
