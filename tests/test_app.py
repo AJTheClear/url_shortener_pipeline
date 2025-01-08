d0a8e601e1d5b26b423c381edc1b622c426b6eec
AJTheClear <jasen.bs@gmail.com>
2025-01-09 01:53:59 +0200

    c

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,14 +5,15 @@ from app import app, db, URL
 
 @pytest.fixture
 def client():
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-        'GITHUB_ACTION_DATABASE_URL',
-        "postgresql://jason2:jason2@localhost:5432/test_url_shortener"
-    )
-    app.config['TESTING'] = True
 
     with app.test_client() as test_client:
         with app.app_context():
+            app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+                'GITHUB_ACTION_DATABASE_URL',
+                "postgresql://jason2:jason2@localhost:5432/url_shortener"
+                )
+            app.config['TESTING'] = True
+
             print("Starting drop_all()")
             db.drop_all()
             print("Finished drop_all(), starting upgrade()")
